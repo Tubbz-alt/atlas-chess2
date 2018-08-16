fbdf2eabb2ebcb27491dd1b812ce4a3bfb10b838
hanyubo <hanyubo@slac.stanford.edu>
2018-08-16 13:39:01 -0700

    functions for stream readout

--- software/scripts/FebGui.py
+++ software/scripts/FebGui.py
@@ -66,7 +66,7 @@ class System(pyrogue.Root):
         self.add(AtlasChess2Feb.feb(memBase=srp))
 
         # Add run cotnrol
-        self.add(pyrogue.RunControl(name = 'runControl', description='Run Controller Chess 2', cmd=self.Trigger, rates={1:'1 Hz', 2:'2 Hz', 4:'4 Hz', 8:'8 Hz', 10:'10 Hz', 30:'30 Hz', 60:'60 Hz', 120:'120 Hz'}))
+        self.add(pyrogue.RunControl(name = 'runControl', description='Run Controller Chess 2', cmd=self.Trigger, rates={1:'1 Hz', 2:'2 Hz', 4:'4 Hz', 8:'8 Hz', 10:'10 Hz', 30:'30 Hz', 60:'60 Hz', 120:'120 Hz', 1000:'1000 Hz'}))
 
 
 # Add data stream to file as channel 1 File writer
